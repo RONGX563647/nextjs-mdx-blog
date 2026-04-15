042be30e248aa33b10a8521a93dbd79d67700350
LiuRongXiang <lrx563647@Outlook.com>
2026-04-15 23:12:35 +0800

    docs:文档更新

--- next.config.js
+++ next.config.js
@@ -4,7 +4,17 @@ const nextConfig = {
   experimental: {
     mdxRs: true,
   },
-  pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx']
+  pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      // 排除 public/md 下的构建产物
+      config.watchOptions = {
+        ...config.watchOptions,
+        ignored: ['**/public/md/**/build/**', '**/public/md/**/CMakeFiles/**'],
+      }
+    }
+    return config
+  },
 }
 
 module.exports = nextConfig
